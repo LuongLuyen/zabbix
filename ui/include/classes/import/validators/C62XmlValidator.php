31aebe4f1586ec00d33f0a65cc866f6975edbe47
Duylnbg02 <duylnbg02@gmail.com>
2026-01-08 03:38:40 +0000

    feat:change sdnet name 4

--- ui/include/classes/import/validators/C62XmlValidator.php
+++ ui/include/classes/import/validators/C62XmlValidator.php
@@ -955,7 +955,7 @@ class C62XmlValidator extends CXmlValidatorGeneral {
 							'name' =>					['type' => XML_STRING | XML_REQUIRED],
 							'delay' =>					['type' => XML_STRING, 'default' => '1m'],
 							'attempts' =>				['type' => XML_STRING, 'default' => '1'],
-							'agent' =>					['type' => XML_STRING, 'default' => 'Zabbix'],
+							'agent' =>					['type' => XML_STRING, 'default' => 'SDNet'],
 							'http_proxy' =>				['type' => XML_STRING, 'default' => ''],
 							'variables' =>				['type' => XML_INDEXED_ARRAY, 'prefix' => 'variable', 'rules' => [
 								'variable' =>				['type' => XML_ARRAY, 'rules' => [
@@ -1642,7 +1642,7 @@ class C62XmlValidator extends CXmlValidatorGeneral {
 							'name' =>					['type' => XML_STRING | XML_REQUIRED],
 							'delay' =>					['type' => XML_STRING, 'default' => '1m'],
 							'attempts' =>				['type' => XML_STRING, 'default' => '1'],
-							'agent' =>					['type' => XML_STRING, 'default' => 'Zabbix'],
+							'agent' =>					['type' => XML_STRING, 'default' => 'SDNet'],
 							'http_proxy' =>				['type' => XML_STRING, 'default' => ''],
 							'variables' =>				['type' => XML_INDEXED_ARRAY, 'prefix' => 'variable', 'rules' => [
 								'variable' =>				['type' => XML_ARRAY, 'rules' => [
